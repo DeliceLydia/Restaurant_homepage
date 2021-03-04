24961f6968c40d1ee2b61baa5f61054a278caa80
delicelydia <Delydia84@gmail.com>
2021-03-04 11:20:00 +0200

    Added about div in the content div
    - Added a h3 in the about div
    - Added paragraph

--- src/home.js
+++ src/home.js
@@ -1,11 +1,21 @@
 const home = (content) => {
-  const body = document.querySelector('#body');
-  const head = document.createElement('h1');
-  const image = document.createElement('img');
-  head.textContent = "Come and taste!";
+  const body = document.querySelector("#body");
+  const head = document.createElement("h1");
+  const div = document.createElement("div");
+  div.setAttribute("id", "about");
+  const aboutUs = document.createElement("h3");
+  const paragraph = document.createElement("p");
+  const image = document.createElement("img");
+  head.textContent = "Come and taste our delicious african food!";
+  aboutUs.textContent = "About our restaurant!";
+  paragraph.textContent =
+    "Our restaurant have differents types of foods but our speciality is african so if you feel like traveling in africa without taking a trip visit us!";
   image.src = "/images/meal.png";
   body.appendChild(image);
   content.appendChild(head);
+  div.appendChild(aboutUs);
+  div.appendChild(paragraph);
+  body.appendChild(div);
 };
 
 export default home;
